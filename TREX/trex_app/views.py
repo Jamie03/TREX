91384aaaaea06093c96debd0f61a35e2770020d9
Jamie03 <jmsantiagoiii@gmail.com>
2019-03-26 15:48:11 +0800

    fixed views.py and index

--- TREX/trex_app/views.py
+++ TREX/trex_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 #import numpy as np
 from sklearn import linear_model
 import json
+from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -10,7 +12,14 @@ def index(request):
     return render(request, 'trex_app/index.html')
 
 def createmodel(request):
-    print('hello world')
-    print(request.POST['code'])
-    print(exec(request.POST['code'])) #Pass this through JSON
-    return json.dumps(exec(request.POST['code']))
+    #print('hello world')
+    #print(request.POST['code'])
+    def my_exec(code):
+        exec('global i; i = %s' % code)
+        global i
+        return i
+
+    data = my_exec(request.POST['code'])
+
+    #print(exec(request.POST['code'])) #Pass this through JSON
+    return HttpResponse(json.dumps(data), content_type='application/json')
